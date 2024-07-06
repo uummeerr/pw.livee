3696612315df4ba215a626018d71b38a06da60e9
umerrahim <167948749+uummeerr@users.noreply.github.com>
2024-07-06 14:16:44 +0000

    js basics

---
+++ onenew/01basics.js
@@ -0,0 +1,16 @@
+const accountId = 1000
+let accountemail = "umerahim@gmail.com"
+var accountpasword = 123
+accountcity = "jaipur"
+let hello;
+
+// accountId = 2 not allowed
+
+accountemail = "hc@hc.com"
+accountpasword = "asdf"
+accountcity = "bengalaru"
+
+
+console.log(accountId);
+console.log(accountpasword);
+console.table([accountemail, accountpasword,  accountcity, hello])
